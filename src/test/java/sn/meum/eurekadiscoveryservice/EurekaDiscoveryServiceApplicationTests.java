a9dfbac90b18bfa501abb03aa34ffa03bdca9d1c
Meumo <mamadou-d@hotmail.com>
2022-05-13 19:02:36 +0000

    First commit

---
+++ src/test/java/sn/meum/eurekadiscoveryservice/EurekaDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.meum.eurekadiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
